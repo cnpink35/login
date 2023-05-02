371d189618c0f534edebe7130035a4d2ba269a14
cnpink35 <33958963+cnpink35@users.noreply.github.com>
2023-05-02 17:05:42 +0900

    Add files via upload

--- userCheck.js
+++ userCheck.js
@@ -7,27 +7,33 @@
 "use strict" //for protection and better debugging
  
 const express = require('express');
-const app = express.Router();
+const app = express();
+	
+//var btn = document.getElementsByClassName(".btn_login");
 
-const user = {
+//btn.addEventListener("click", loginCheck);
+
+function loginCheck(){
+	const user = {
 	id: ["deyoo"],
 	password: ["qwe123!"]
 };
-	
-var btn = document.getElementsByClassName(".btn_login");
 
-btn.addEventListener("click", loginCheck);
-
-function loginCheck(){
-	const id = document.querySelector("#id");
-	const pw = document.querySelector("#pw");
+	const id = document.querySelector("#id").value;
+	const pw = document.querySelector("#pw").value;
 	
 	if(user.id.includes(id)){
 		const idx = user.id.indexOf(id);
 		if(pw===user.password[idx]){
 			alert("login success!");
+			
+			$(".id_pw_wrap").style.display = "none";
+			$(".login_success").style.display = "block";
+			return;
 		}
 	} else{
 		alert("id or pw is wrong!");
+		$("#err_common > .error_message").textContent = "id or pw is wrong!";
+  		$("#err_common").style.display = "block";
 	}
 }
